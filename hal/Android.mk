7afd716f0c1ed762866f6766cf2932f437252b71
Max Weninger <max.weninger@gmail.com>
2018-03-25 13:22:21 +0200

    Merge "hal: proximity fix for chiron" into android-8.1

--- hal/Android.mk
+++ hal/Android.mk
@@ -367,6 +367,11 @@ ifeq ($(strip $($AUDIO_FEATURE_IP_HDLR_ENABLED)),true)
     LOCAL_SRC_FILES += audio_extn/ip_hdlr_intf.c
 endif
 
+ifeq ($(strip $(AUDIO_FEATURE_ELLIPTIC_ULTRASOUND_SUPPORT)),true)
+    LOCAL_CFLAGS += -DELLIPTIC_ULTRASOUND_ENABLED
+    LOCAL_SRC_FILES += audio_extn/ultrasound.c
+endif
+
 LOCAL_CFLAGS += -Wall -Werror
 LOCAL_CFLAGS += -Wno-error=unused-function -Wno-error=unused-label -Wno-error=unused-value -Wno-error=unused-variable 
 
